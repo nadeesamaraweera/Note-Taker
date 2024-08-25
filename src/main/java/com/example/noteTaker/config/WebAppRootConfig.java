48cbe489551d5ca84026b3d392fdc65b002e7eb4
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-25 16:29:23 +0530

    Create Model mapper based utility for entity dto mapping and vice versa

--- src/main/java/com/example/noteTaker/config/WebAppRootConfig.java
+++ src/main/java/com/example/noteTaker/config/WebAppRootConfig.java
@@ -1,6 +1,7 @@
 package com.example.noteTaker.config;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +23,14 @@ import javax.sql.DataSource;
 @EnableWebMvc
 @EnableJpaRepositories
 @EnableTransactionManagement
+
+
 public class WebAppRootConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     @Bean
     public DataSource dataSource() {
--- src/main/java/com/example/noteTaker/util/AppUtil.java
+++ src/main/java/com/example/noteTaker/util/AppUtil.java
@@ -1,9 +1,17 @@
 package com.example.noteTaker.util;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.UUID;
 
 public class AppUtil {
+
+
     public static String createNote(){
         return "NOTE "+ UUID.randomUUID().toString();
     }
 }
+
+//mapping
+//note details
