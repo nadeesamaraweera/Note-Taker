ad08b4c3e562600c9a98f891a9b5dd0e5e3d26d4
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-09-01 15:25:17 +0530

    User DTO and Entity mapping completed

--- src/main/java/com/example/noteTaker/util/Mapping.java
+++ src/main/java/com/example/noteTaker/util/Mapping.java
@@ -1,7 +1,9 @@
 package com.example.noteTaker.util;
 
 import com.example.noteTaker.dto.NoteDTO;
+import com.example.noteTaker.dto.UserDTO;
 import com.example.noteTaker.entity.NoteEntity;
+import com.example.noteTaker.entity.UserEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class Mapping { //dto entity waltth entity dto wltth convert karanna.
+public class Mapping { //dto entity waltth entity dto wltth convert karanna.  //anith layers athre data gnne dto wlin.
 
     @Autowired
     private ModelMapper modelMapper;
@@ -28,4 +30,16 @@ public class Mapping { //dto entity waltth entity dto wltth convert karanna.
         return modelMapper.map(notes,List.class);
     }
 
+
+    //user matters mapping
+
+    public UserEntity convertToUserEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, UserEntity.class);
+    }
+    public UserDTO convertToUserDTO(UserEntity userEntity) {
+        return modelMapper.map(userEntity, UserDTO.class);
+    }
+    public List<UserDTO> convertUserToDTO(List<UserEntity> userEntities) {
+        return modelMapper.map(userEntities, List.class);
+    }
 }
